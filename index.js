fe06ee93ff2924d18d7b0da5ae50ff56b7fcbc7f
gxdujjwalxbot <pokka3011@gmail.com>
2026-07-02 06:05:35 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const { makeWASocket, useMultiFileAuthState } = require('@whiskeysockets/baileys');
+const TelegramBot = require('node-telegram-bot-api');
+
+const tgBot = new TelegramBot('8875447814:AAFWdMN5Wot8AGt6u5pn_fP53IkofxcT7CE', { polling: true });
+
+tgBot.onText(/\/start/, async (msg) => {
+    const { state, saveCreds } = await useMultiFileAuthState(`./sessions/${msg.chat.id}`);
+    const sock = makeWASocket({ auth: state });
+
+    const code = await sock.requestPairingCode('918414938563'); // Yahan apna WhatsApp number likhna
+    tgBot.sendMessage(msg.chat.id, `Pairing Code: ${code}`);
+    sock.ev.on('creds.update', saveCreds);
+});
+
+console.log("Bot Online!");
