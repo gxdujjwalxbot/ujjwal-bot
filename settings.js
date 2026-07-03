92c86805feab2e983515ea3dbc4f78447e149770
gxdujjwalxbot <pokka3011@gmail.com>
2026-07-04 04:46:10 +0530

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'gxd_ujjwalsbot',
+  author: '‎',
+  botName: "6nine4 X Ujjwals bot",
+  botOwner: 'MR UJJWAL', //Your name
+  ownerNumber: '918414938563', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.7",
+  updateZipUrl: "https://github.com/ujjwalsmd/gxd_ujjwalbot.git",
+};
+
+module.exports = settings;
